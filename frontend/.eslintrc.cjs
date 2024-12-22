d2291f9eba560a53a14b2b43e34148fbe193d1f4
linuxd3v <linuxd3v@users.noreply.github.com>
2024-12-22 20:23:46 +0000

    formatting: formatted the file

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -1,7 +1,7 @@
 module.exports = {
-    env: { browser: true, es2020: true },
-    extends: [
-      'eslint:recommended',
-    ],
-    parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  }
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+  ],
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+}
