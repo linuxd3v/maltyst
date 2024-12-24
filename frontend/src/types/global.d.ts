ec18af2b88ee8634939f36aa78cb00287b36971c
linuxd3v <linuxd3v@users.noreply.github.com>
2024-12-24 22:53:39 +0000

    formatting: 2 spaces formatting

--- frontend/src/types/global.d.ts
+++ frontend/src/types/global.d.ts
@@ -1,24 +1,23 @@
-// src/types/global.d.ts
 export {};
 
 declare global {
-    interface Window {
-        maltyst?: {
-            maltystContactUqid?: string | null;
-            inProgressOptin?: boolean;
-            inProgressPcPulling?: boolean;
-            inProgressPcSubmission?: boolean;
-        };
-        maltystData: {
-            nonce: string;
-            MALTYST_PREFIX: string;
-            MALTYST_ROUTE: string;
-        };
-    }
+  interface Window {
+      maltyst?: {
+          maltystContactUqid?: string | null;
+          inProgressOptin?: boolean;
+          inProgressPcPulling?: boolean;
+          inProgressPcSubmission?: boolean;
+      };
+      maltystData: {
+          nonce: string;
+          MALTYST_PREFIX: string;
+          MALTYST_ROUTE: string;
+      };
+  }
 }
 
 
 export interface ApiResponse {
-    message?: string;
-    error?: string;
+  message?: string;
+  error?: string;
 }
